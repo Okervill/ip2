a904705375d7671da9c7d18f8c0d22f3f149b136
Zubair <zubairkhalid12@gmail.com>
2020-02-28 15:47:17 +0000

    added proper adminhome fxml

--- ip2/src/AdminHomePage/AdminHomeController.java
+++ ip2/src/AdminHomePage/AdminHomeController.java
@@ -17,7 +17,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
