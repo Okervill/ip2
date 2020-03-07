b8a8338a707f1f6d213ee7b0558d86a915e46ad9
erinoconnor <erinoconnor2000@outlook.com>
2020-03-07 01:34:57 +0000

    add question changed
    added stylesheets and new fxml for add question

--- ip2/src/QuestionPage/ViewQuestionsController.java
+++ ip2/src/QuestionPage/ViewQuestionsController.java
@@ -7,6 +7,7 @@ package QuestionPage;
 
 import SQL.SQLHandler;
 import ip2.Question;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
@@ -25,6 +26,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.effect.DropShadow;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -41,6 +43,7 @@ public class ViewQuestionsController implements Initializable {
     private TableColumn<Question, String> col_quest;
     @FXML
     private TableColumn<Question, String> col_answer;
+  
 
     /**
      * Initializes the controller class.
@@ -71,8 +74,7 @@ public class ViewQuestionsController implements Initializable {
 
         col_quest.setCellValueFactory(new PropertyValueFactory<>("UserQuestion"));
         col_answer.setCellValueFactory(new PropertyValueFactory<>("CorrectAnswer"));
-        
-        
+
     }
 
     @FXML
@@ -83,7 +85,6 @@ public class ViewQuestionsController implements Initializable {
         Question item = table.getItems().get(index);
 
         String quest = (String) col_quest.getCellObservableValue(item).getValue();
-     
 
         ArrayList<String> allQuestions = new ArrayList<>();
 
@@ -121,5 +122,23 @@ public class ViewQuestionsController implements Initializable {
         currentQuestion.deleteQuestion(currentQuestion);
 
     }
+    
+     @FXML
+    private void addQuestion(ActionEvent event) throws IOException, SQLException {
+        
+          Parent root;
+        root = FXMLLoader.load(getClass().getResource("AddQuestion.fxml"));
+
+        Scene scene = new Scene(root);
+        Stage add = new Stage(StageStyle.DECORATED);
+        add.setTitle("Add Question");
+        add.setScene(scene);
+
+        add.show();
+        ((Stage) (((Button) event.getSource()).getScene().getWindow())).close();
+    }
+   
+
+
 
 }
