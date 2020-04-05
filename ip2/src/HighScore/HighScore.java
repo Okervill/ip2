d80c586c50ac3f10a35f8b94587e27febb97607e
erinoconnor <erinoconnor2000@outlook.com>
2020-04-06 00:18:42 +0100

    fxmls

---
+++ ip2/src/HighScore/HighScore.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HighScore;
+
+import ip2.User;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Patrick
+ */
+public class HighScore extends Application {
+
+    User currentUser;
+
+    public HighScore(User user) {
+        currentUser = user;
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/HighScore/HighScore.fxml"));
+        Parent root = (Parent) loader.load();
+
+        HighScoreController controller = loader.getController();
+
+        controller.setData(currentUser);
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("Home");
+
+        stage.show();
+        stage.centerOnScreen();
+
+    }
+
+}
